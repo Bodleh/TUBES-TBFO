87402cc119591aae8aa2cb53d44d616c436b887f
Juan A <140235896+juanaw6@users.noreply.github.com>
2023-11-18 23:50:33 +0700

    feat: Add argparse, handle argparse error

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-file_path = 'test.html'
+import argparse
 
-with open(file_path, 'r', encoding='utf-8') as file:
-    while True:
-        char = file.read(1)
-        if char == " " or char == "\n" :
-            continue
-        if not char :
-            break
-        print(char)
+tokens = []
+states = []
+tf = []
+
+def parse_dfa(file_pda: str) -> bool :
+    global tf
+    print(file_pda) 
+
+def parse_html(file_html) :
+    global tokens
+    #parse
+    print(file_html)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('file1', type=str, help='The path to the PDA definition file (.txt)')
+parser.add_argument('file2', type=str, help='The path to the HTML file (.html)')
+
+args = parser.parse_args()
+file_pda, file_html = args.file1, args.file2
+
+is_txt, is_html = file_pda.endswith('.txt'), file_html.endswith('.html')
+if not is_txt :
+    print("Error: File 1 is not a txt file")
+
+if not is_html :
+    print("Error: File 2 is not a HTML file")
+
+if not is_txt or not is_html :
+    exit()
+
+file_pda = open(file_pda, 'r').read()
+file_html = open(file_html, 'r').read()
+
+success_parse_pda = parse_dfa(file_pda)
+parse_html(file_html)
--- test.py
+++ test.py
@@ -1,1 +1,40 @@
-print("a")
+import argparse
+
+tokens = []
+states = []
+tf = []
+
+def parse_dfa(file_pda: str) -> bool :
+    global tf
+    print(file_pda) 
+
+def parse_html(file_html) :
+    global tokens
+    chars_to_remove = [' ', '\n']
+    regex_pattern = '[' + ''.join(chars_to_remove) + ']'
+    file_html = file_html.replace('\n', '').split(' ')
+    print(file_html)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('file1', type=str, help='The path to the PDA definition file (.txt)')
+parser.add_argument('file2', type=str, help='The path to the HTML file (.html)')
+
+args = parser.parse_args()
+file_pda, file_html = args.file1, args.file2
+
+is_txt, is_html = file_pda.endswith('.txt'), file_html.endswith('.html')
+if not is_txt :
+    print("Error: File 1 is not a txt file")
+
+if not is_html :
+    print("Error: File 2 is not a HTML file")
+
+if not is_txt or not is_html :
+    exit()
+
+file_pda = open(file_pda, 'r').read()
+file_html = open(file_html, 'r').read()
+
+success_parse_pda = parse_dfa(file_pda)
+parse_html(file_html)
