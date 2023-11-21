eeab237f399bd67670e07284f079b30b1cfe7eb7
AlbertChoe <albert.choe73@gmail.com>
2023-11-21 21:43:12 +0700

    feat : done html parser and fixing parser bug

--- htmlparser.py
+++ htmlparser.py
@@ -1,43 +1,55 @@
 import re
 
-def parse_html(html_path) -> list:
-    with open(html_path, 'r') as file:
-        html_content = file.read()
-    
+
+def parse_html(html_content):
     tag_regex = r"(<!--[^>]*-->)|(<[^>]+>)|([^<]+)"
     tokens = []
+    special_values = ['get', 'post', 'submit', 'reset', 'button',
+                      'password', 'text', 'email', 'number', 'checkbox']
+    special_tags = {'button': ['type'], 'form': ['method'], 'input': ['type']}
 
     for match in re.finditer(tag_regex, html_content):
         if match.group(1):  # If it's a comment
             comment = match.group(1)
-            # Split the comment into start, content, and end
             if comment.startswith('<!--') and comment.endswith('-->'):
-                tokens.append('<!--' + comment[4:-3].strip())
+                tokens.append('<!--')
+                tokens.append('STR')  # Comment content as 'STR'
                 tokens.append('-->')
             else:
-                # Handle cases with spaces like <! --comment -- >
                 comment_parts = re.split(
                     r'(\s*<!\s*|\s*--\s*|\s*>|\s*--\s*>)', comment)
                 tokens.extend([part for part in comment_parts if part.strip()])
         elif match.group(2):  # If it's a tag
             tag = match.group(2)
+            tag_name = ''
+            attribute_name = ''
             inside_quotes = False
             quote_content = ''
             temp_token = ''
 
             for char in tag:
-                if char == '"' and not inside_quotes:  # Opening quote
+                if char == '"' and not inside_quotes:
                     inside_quotes = True
-                    if temp_token:  # Add the token before the quote
-                        tokens.append(temp_token)
-                        temp_token = ''
-                    tokens.append('"')  # Add the opening quote
-                    continue
-                elif char == '"' and inside_quotes:  # Closing quote
-                    inside_quotes = False
-                    tokens.append("STR" if quote_content.strip() else "NO_STR")
-                    tokens.append('"')  # Add the closing quote
                     quote_content = ''
+                    tokens.append('"')  # Add opening quote to tokens
+                    continue
+
+                if char == '"' and inside_quotes:
+                    inside_quotes = False
+                    cleaned_tag_name = tag_name.split('<')[-1]
+
+                    if cleaned_tag_name in special_tags and attribute_name in special_tags[cleaned_tag_name]:
+                        if quote_content in special_values:
+                            tokens.append(quote_content)
+                        else:
+                            tokens.append(
+                                "STR" if quote_content.strip() else "NO_STR")
+                    else:
+                        tokens.append(
+                            "STR" if quote_content.strip() else "NO_STR")
+
+                    attribute_name = ''
+                    tokens.append('"')  # Add closing quote to tokens
                     continue
 
                 if inside_quotes:
@@ -45,23 +57,44 @@ def parse_html(html_path) -> list:
                 else:
                     if char in [' ', '=', '>', '\n', '\r', '\t']:
                         if temp_token:
+                            if temp_token not in ['<', '=', '>']:
+                                if not tag_name:
+                                    tag_name = temp_token
+                                else:
+                                    attribute_name = temp_token
                             tokens.append(temp_token)
                             temp_token = ''
-                        if char == '=':
-                            tokens.append(char)
-                        elif char == '>':
+                        if char in ['=', '>']:
                             tokens.append(char)
                     elif char != '<':
                         temp_token += char
                     else:
                         temp_token = char
 
-            if temp_token:  # Add the last token if it exists
+            if temp_token:
                 tokens.append(temp_token)
 
         else:  # If it's text content
             text = match.group(3).strip()
-            if text:  # If the text is not just whitespace
-                tokens.append("STR")  # Append 'STR' for non-whitespace text
+            if text:
+                tokens.append("STR")
 
     return tokens
+
+
+def filter_tokens(tokens):
+    # Filter out 'STR' and 'NO_STR' from the token list
+    filtered_tokens = [
+        token for token in tokens if token not in ['STR', 'NO_STR']]
+    return filtered_tokens
+
+
+# Read the HTML file
+file_html = "testuntukerror.html"
+with open(file_html, 'r') as file:
+    html_content = file.read()
+
+# Parse the HTML and get tokens
+tokens = parse_html(html_content)
+tokens = filter_tokens(tokens)
+print(tokens)
